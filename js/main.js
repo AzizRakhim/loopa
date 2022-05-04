52cd61053c66730d52eae806e6a920c560d0904a
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-05-04 20:29:39 +0500

    styled it

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 let elImgHolder = document.querySelector(".img-holder");
 let elCircleImg = document.querySelector(".circle-box img");
+let elCircleBox = document.querySelector(".circle-box");
 
 elImgHolder.addEventListener("mousemove", (e) => {
-  let x = e.offsetX - 50;
-  let y = e.offsetY - 60;
+  elCircleBox.style.display = "block";
+  let x = e.offsetX;
+  let y = e.offsetY;
   elCircleImg.style.transform = `translate(-${x}px, -${y}px) scale(2)`;
-})
+});
+
+elImgHolder.addEventListener("mouseleave", () => {
+  elCircleBox.style.display = "none";
+});
